240ddca806495e927d3ff68e6688256b9958b943
Freitas <adavidaf2002@gmail.com>
2023-12-09 14:18:40 +0000

    lame

---
+++ cmake-build-debug/CMakeFiles/AirBusManagementSystem.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "AirBusManagementSystem"
+  "AirBusManagementSystem.pdb"
+  "CMakeFiles/AirBusManagementSystem.dir/classes/Parser.cpp.o"
+  "CMakeFiles/AirBusManagementSystem.dir/classes/Parser.cpp.o.d"
+  "CMakeFiles/AirBusManagementSystem.dir/classes/airline.cpp.o"
+  "CMakeFiles/AirBusManagementSystem.dir/classes/airline.cpp.o.d"
+  "CMakeFiles/AirBusManagementSystem.dir/classes/airport.cpp.o"
+  "CMakeFiles/AirBusManagementSystem.dir/classes/airport.cpp.o.d"
+  "CMakeFiles/AirBusManagementSystem.dir/main.cpp.o"
+  "CMakeFiles/AirBusManagementSystem.dir/main.cpp.o.d"
+  "CMakeFiles/AirBusManagementSystem.dir/network/edge.cpp.o"
+  "CMakeFiles/AirBusManagementSystem.dir/network/edge.cpp.o.d"
+  "CMakeFiles/AirBusManagementSystem.dir/network/graph.cpp.o"
+  "CMakeFiles/AirBusManagementSystem.dir/network/graph.cpp.o.d"
+  "CMakeFiles/AirBusManagementSystem.dir/network/vertex.cpp.o"
+  "CMakeFiles/AirBusManagementSystem.dir/network/vertex.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AirBusManagementSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

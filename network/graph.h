ff4431cdfd70970afad7bb3ba62eae29093fa437
Freitas <adavidaf2002@gmail.com>
2023-12-09 17:02:11 +0000

    Fixed distance feature

--- network/graph.h
+++ network/graph.h
@@ -56,7 +56,7 @@ class Vertex {
 public:
     explicit Vertex(int id);
     int getId();
-    double getDistance() const;
+    double getDistance();
     Airport getAirport();
     void setAirport(Airport in);
     bool isVisited() const;

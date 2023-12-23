7fd82c7d7273893665396b54af595508b561810c
Freitas <adavidaf2002@gmail.com>
2023-12-23 15:05:12 +0000

    Updates vertex.cpp

--- network/vertex.cpp
+++ network/vertex.cpp
@@ -17,10 +17,6 @@ Airport Vertex::getAirport() {
     return airport;
 }
 
-void Vertex::setAirport(Airport in) {
-    Vertex::airport = in;
-}
-
 bool Vertex::isProcessing() const {
     return processing;
 }
@@ -29,26 +25,9 @@ void Vertex::setProcessing(bool p) {
     Vertex::processing = p;
 }
 
-/*
- * Auxiliary function to add an outgoing edge to a vertex (this),
- * with a given destination vertex (d) and edge weight (w).
- */
+
 void Vertex::addEdge(Vertex *d, const Airline& airline, double w) {
     adj.emplace_back(d, airline, w);
-}
-
-/*
- * Auxiliary function to remove an outgoing edge (with a given destination (d))
- * from a vertex (this).
- * Returns true if successful, and false if such edge does not exist.
- */
-bool Vertex::removeEdgeTo(Vertex *d) {
-    for (auto it = adj.begin(); it != adj.end(); it++)
-        if (it->dest  == d) {
-            adj.erase(it);
-            return true;
-        }
-    return false;
 }
 
 

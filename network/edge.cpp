c5f67a85c2bcf66029fbe975849c561ac5d9bb49
Freitas <adavidaf2002@gmail.com>
2023-12-23 15:03:28 +0000

    Updates edge.cpp

--- network/edge.cpp
+++ network/edge.cpp
@@ -2,24 +2,18 @@
 // Created by Utilizador on 03/12/2023.
 //
 
+#include <utility>
+
 #include "graph.h"
 
-Edge::Edge(Vertex *d, Airline airline, double w) : dest(d), weight(w), airline(airline) {}
+Edge::Edge(Vertex *d, Airline airline, double w) : dest(d), weight(w), airline(std::move(airline)) {}
 
 Vertex *Edge::getDest() const {
     return dest;
 }
 
-void Edge::setDest(Vertex *d) {
-    Edge::dest = d;
-}
-
 double Edge::getWeight() const {
     return weight;
-}
-
-void Edge::setWeight(double weight) {
-    Edge::weight = weight;
 }
 
 Airline Edge::getAirline() const{

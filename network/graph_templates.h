e5bd1cfb7d921a135282e1ace74645328466d52b
Freitas <adavidaf2002@gmail.com>
2023-12-27 20:27:11 +0000

    Refactored

--- network/graph_templates.h
+++ network/graph_templates.h
@@ -10,37 +10,37 @@
 template<typename Container>
 Container Graph::listReachableEntities(int v, int max) {
     for (int i = 0; i < getNumVertex(); i++)
-        vertexSet[i]->setVisited(false);
+        vertexSet[i].setVisited(false);
 
     Container entities;
 
     queue<int> q;
     q.push(v);
-    vertexSet[v]->setVisited(true);
-    vertexSet[v]->distance = 0;
+    vertexSet[v].setVisited(true);
+    vertexSet[v].distance = 0;
 
     while (!q.empty()) {
         int u = q.front(); q.pop();
-        for (const auto& e : vertexSet[u]->getAdj()) {
+        for (const auto& e : vertexSet[u].getAdj()) {
 
-            int w = e.dest->getId();
+            int w = e.dest;
 
-            if (!vertexSet[w]->isVisited()) {
+            if (!vertexSet[w].isVisited()) {
 
                 q.push(w);
-                vertexSet[w]->setVisited(true);
-                vertexSet[w]->distance = vertexSet[u]->getDistance() + 1;
+                vertexSet[w].setVisited(true);
+                vertexSet[w].distance = vertexSet[u].getDistance() + 1;
 
-                if (vertexSet[w]->getDistance() <= max) {
+                if (vertexSet[w].getDistance() <= max) {
 
                     if constexpr (std::is_same<Container, Airport::AirportH>::value) {
-                        entities.insert(vertexSet[w]->getAirport());
+                        entities.insert(vertexSet[w].getAirport());
                     }
                     else if constexpr (std::is_same<Container, Airport::CityH2>::value) {
-                        entities.insert({vertexSet[w]->getAirport().getCountry(), vertexSet[w]->getAirport().getCity()});
+                        entities.insert({vertexSet[w].getAirport().getCountry(), vertexSet[w].getAirport().getCity()});
                     }
                     else if constexpr (std::is_same<Container, std::set<std::string>>::value) {
-                        entities.insert(vertexSet[w]->getAirport().getCountry());
+                        entities.insert(vertexSet[w].getAirport().getCountry());
                     }
                 }
             }
